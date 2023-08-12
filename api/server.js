9a681d17f3775fabe731337c12cd3cfd9691eaed
swarnkartannuverma <tannuswarnkarverma4@gmail.com>
2023-08-12 18:22:55 +0530

    new changes

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
+
 // JSON Server module
 const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("./db.json");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
